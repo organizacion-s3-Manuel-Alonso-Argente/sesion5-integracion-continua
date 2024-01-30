f676dc965fd3f63b6253064aef64b6a571ed9a5a
Manuel Alonso Argente <145504941+manaloarg@users.noreply.github.com>
2024-01-30 08:12:47 +0100

    Update operaciones.py incluir es_pas

--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -3,3 +3,11 @@ def suma(x,y):
 
     # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente
     return x + y
+
+#Función para saber si un número es par o impar
+def es_par(x):
+    #Si el resto de la división da 0 es porque el número es par.
+    if x%2 == 0:
+        return "par"
+    else:
+        return "impar"
